21aba879df41921244d44f11d553b59ac5cb42fc
François Belleau <francois.belleau@gmail.com>
2021-01-15 11:27:51 -0500

    Update resid_wsdl2es.py

--- stuff2json-ld/resid/resid_wsdl2es.py
+++ stuff2json-ld/resid/resid_wsdl2es.py
@@ -9,7 +9,7 @@ import json
 
 from elasticsearch import Elasticsearch
 es = Elasticsearch("http://192.168.3.46:9209")
-es = Elasticsearch("http://sirenadmin:password@206.12.89.13:9200")
+es = Elasticsearch("http://***@206.12.89.13:9200")
 print(es)
 
 xmlDoc = minidom.parse('RESIDUES.XML')
